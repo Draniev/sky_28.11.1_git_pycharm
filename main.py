a5abc48c7a2ca482040337b1716b868c893cbb07
Sergei Popov <sergei.v.popov@icloud.com>
2023-12-05 23:16:02 +0300

    call subtraction_to_vars in main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from funcs import summ_to_vars
+from funcs import summ_to_vars, subtraction_to_vars
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print(summ_to_vars(5, 5))
+    subtraction = subtraction_to_vars(10, 5)
+    print(subtraction)
